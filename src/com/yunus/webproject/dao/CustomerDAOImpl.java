6d0b69475f214044c6fa06ea14a25bf3e88fa926
Yunus Bağrıyanık <45238344+yunusbgrynk@users.noreply.github.com>
2019-02-19 05:39:55 +0300

    Update CustomerDAOImpl.java

--- src/com/yunus/webproject/dao/CustomerDAOImpl.java
+++ src/com/yunus/webproject/dao/CustomerDAOImpl.java
@@ -20,7 +20,6 @@ public class CustomerDAOImpl implements CustomerDAO {
 	
 	
 	@Override
-	//@Transactional //delete this because, we've moved this functionality to the service layer.
 	public List<Customer> getCustomers() {
 		
 		// get the current hibernate session
